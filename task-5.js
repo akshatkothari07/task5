a00d53e763c1ef56bbaf2eaa104fd58bd41f534b
akshat kothari <81756820+akshatkothari07@users.noreply.github.com>
2021-04-14 23:43:28 +0530

    Add files via upload

---
+++ task-5.js
@@ -0,0 +1,32 @@
+var foto;
+window.onload= function() {
+    foto = new Foto();
+}
+
+function selectImage(){
+    document.getElementById("foto-file").click();
+}
+function makeGrayScale(){
+    foto.grayscale();
+}
+function makeBright(){
+    foto.makeBright();
+}
+function makeDark(){
+    foto.makeDark();
+}
+function makeBlur(){
+    foto.applyBlurFilter();
+}
+function makeEmboss(){
+    foto.applyEmbossFilter();
+}
+function makeSharp(){
+    foto.applySharpfilter();
+}
+
+function download()
+{
+    foto.export();
+}
+
